9713b1ef32a37b0d8973aff788e380fb9c98d412
Mark Broihier <mbroihier@yahoo.com>
2020-06-27 20:36:10 +0000

    fix error propagation

--- Model.py
+++ Model.py
@@ -1,23 +1,33 @@
-import NeuralLayer as nl
 import tensorflow as tf
+import NeuralLayer
+
+
 class Model:
     '''
-    Class for defining a model that consists of nueral layers.  The first layer is always the inputs, which means that it does not exist as a NeuralLayer.
-    All subsequent layers are completely interconnected except for the final layer
+    Class for defining a model that consists of neural layers.  The first layer is always the inputs, which
+    means that it does not exist as a NeuralLayer.  All subsequent layers are completely interconnected
+    except for the final layer.
     '''
-    def __init__(self, inputOutputList, filePath=None):
+    def __init__(self, inputOutputList, debug=False, filePath=None):
         '''
         Model constructor - contrusts layers from the list entries
         '''
         self.layers = []
         layerIndex = 0
-        for (inputs, outputs, learningFactor) in inputOutputList:
+
+        for entryTuple in inputOutputList:
+            layerid = None
+            if isinstance(entryTuple[-1], str):
+                layerid = entryTuple[-1]
+            inputs = entryTuple[0]
+            outputs = entryTuple[1]
+            learningFactor = entryTuple[2]
             if filePath is None:
                 weightFilePath = filePath
             else:
                 weightFilePath = filePath + str(layerIndex)
                 layerIndex += 1
-            self.layers.append(nl.NeuralLayer(inputs, outputs, learningFactor, weightFilePath))
+            self.layers.append(NeuralLayer.NeuralLayer(inputs, outputs, learningFactor, layerid, debug, weightFilePath))
 
     def storeModel(self, filePath):
         '''
@@ -27,7 +37,7 @@ class Model:
         for layer in self.layers:
             layer.storeLayer(filePath + str(layerIndex))
             layerIndex += 1
-        
+
     def feedForward(self, inputs):
         '''
         Given the inputs, propagate them through the model layers
@@ -46,20 +56,18 @@ class Model:
         lastLayer = len(reversedLayers) - 1
         newDeltaList = False
         if deltas is None:
-            deltas = []  #  make a list of deltas, one for each layer
+            deltas = []  # make a list of deltas, one for each layer
             newDeltaList = True
         for index, layer in enumerate(reversedLayers):
-            #print("Working on layer index {}".format(index))
-            #print("updating deltas for Index: {}, layer: {}".format(index, lastLayer - index))
+            topIndicator = index == 0
             if newDeltaList:
-                deltas.append(layer.updateDeltas(target))
+                deltas.append(layer.updateDeltas(target, topIndicator))
             else:
-                deltas[index] = layer.updateDeltas(target, deltas=deltas[index])
+                deltas[index] = layer.updateDeltas(target, topIndicator, deltas=deltas[index])
             if index < lastLayer:
-                reversedLayers[index+1].setPropogationError(layer.errorForNextLayer)
-        #print("returning deltas:\n {}".format(deltas))
+                reversedLayers[index+1].setPropagationError(layer.errorForNextLayer)
         return deltas
-        
+
     def updateWeights(self, target=None, deltas=None):
         '''
         Update the weights and propogte the error of all layers
@@ -68,50 +76,10 @@ class Model:
         reversedLayers.reverse()
         lastLayer = len(reversedLayers) - 1
         for index, layer in enumerate(reversedLayers):
-            #print("Working on layer index {}".format(index))
             if deltas is None:
                 layer.updateWeights(target)
                 if index < lastLayer:
-                    reversedLayers[index+1].setPropogationError(layer.errorForNextLayer)
+                    reversedLayers[index+1].setPropagationError(layer.errorForNextLayer)
             else:
-                #print("type of deltas at this point is: {}".format(type(deltas[index])))
-                #print("updating weights using batch calculated deltas. Index: {}, layer: {}".format(index, lastLayer - index))
                 layer.updateWeights(deltas=deltas[index])
-        
-def main():
-    '''
-    Tests for the class Model
-    '''
-    model = Model([(2,2,0.01), (2,2,0.01)])
-    print("Initial Weights for layer 0:\n {}".format(model.layers[0].weights))
-    print("Initial Weights for layer 1:\n {}".format(model.layers[1].weights))
-    for iteration in range(2000):
-        model.feedForward([1.0, 1.0])
-        model.updateWeights([0.01, 0.99])
 
-    print("Final Weights for layer 0:\n {}".format(model.layers[0].weights))
-    print("Final Weights for layer 1:\n {}".format(model.layers[1].weights))
-    print("model is predicting:\n {}".format(model.feedForward([1.0, 1.0])))  #  lets see how close we got
-    
-    xorModel = Model([(2,2,0.01), (2,2,0.01)])
-    print("Initial Weights for layer 0:\n {}".format(xorModel.layers[0].weights))
-    print("Initial Weights for layer 1:\n {}".format(xorModel.layers[1].weights))
-    for iteration in range(1000):
-        xorModel.feedForward([1.0, 1.0])
-        xorModel.updateWeights([1.0, 0.0])
-        xorModel.feedForward([0.0, 0.0])
-        xorModel.updateWeights([1.0, 0.0])
-        xorModel.feedForward([1.0, 0.0])
-        xorModel.updateWeights([0.0, 1.0])
-        xorModel.feedForward([0.0, 1.0])
-        xorModel.updateWeights([0.0, 1.0])
-
-    print("Final Weights for layer 0:\n {}".format(xorModel.layers[0].weights))
-    print("Final Weights for layer 1:\n {}".format(xorModel.layers[1].weights))
-    print("model is predicting:\n {}".format(xorModel.feedForward([1.0, 1.0])))  #  lets see how close we got
-    print("model is predicting:\n {}".format(xorModel.feedForward([0.0, 0.0])))  #  lets see how close we got
-    print("model is predicting:\n {}".format(xorModel.feedForward([1.0, 0.0])))  #  lets see how close we got
-    print("model is predicting:\n {}".format(xorModel.feedForward([0.0, 1.0])))  #  lets see how close we got
-    
-if __name__ == '__main__':
-    main()
--- NeuralLayer.py
+++ NeuralLayer.py
@@ -1,26 +1,32 @@
 import pickle
 import tensorflow as tf
+
+
 class NeuralLayer:
     '''
     Class for defining a layer of nuerons that can have multiple inputs and neurons/outputs - a neuron can only
     have one output, but it may server as inputs to many neurons in the next layer.  The equations used in this
     class assume that the bias term is included in the weights vector and that the input to the weight is 1.0.
     The equations assume that the loss function is the sum of the squares of the error where error is defined as
-    the difference between a known target value and the output of the Psi function.  The Psi function is 
+    the difference between a known target value and the output of the Psi function.  The Psi function is
     1/(1+e^-z).  And z, also known as net, is the sum of product of the weights and inputs (which includes the bias).
     '''
-    def __init__(self, numberOfInputs, numberOfOutputs, learningFactor=1.0, filePath=None):
+    def __init__(self, numberOfInputs, numberOfOutputs, learningFactor, id="me", debug=False, filePath=None):
         '''
         Nueron constructor - uses tensors - last weight is the bias term
         '''
+        self.id = id
+        self.debug = debug
         self.numberOfInputs = numberOfInputs
         self.numberOfNeurons = numberOfOutputs
         self.backPropagatedErrorNotSet = True
         self.learningFactor = learningFactor
-        self.weights = tf.random.uniform([numberOfInputs+1, numberOfOutputs], minval=-0.5, maxval=0.5, dtype=tf.dtypes.float32)
+        self.delta = [0.0] * self.numberOfNeurons
+        self.weights = tf.random.uniform([numberOfInputs+1, numberOfOutputs], minval=-0.5, maxval=0.5,
+                                         dtype=tf.dtypes.float32)
         self.error = [0.0] * numberOfOutputs
         self.filePath = filePath
-        if not filePath is None:
+        if filePath is not None:
             fileHandle = open(filePath, "rb")
             self.weights = pickle.load(fileHandle)
             fileHandle.close()
@@ -32,12 +38,12 @@ class NeuralLayer:
         fileHandle = open(filePath, "wb")
         pickle.dump(self.weights, fileHandle)
         fileHandle.close()
-        
+
     def calculateOutput(self, inputs):
         '''
         Given the inputs, calculate the outputs
         '''
-        self.inputs = tf.concat([inputs, [1.0]], 0) 
+        self.inputs = tf.concat([inputs, [1.0]], 0)
         self.outputs = self.psi(self.netAKAz())
         return self.outputs
 
@@ -53,13 +59,13 @@ class NeuralLayer:
         '''
         return 1.0 / (1.0 + tf.exp(-z))
 
-    def netWRTWeight (self, index):
+    def netWRTWeight(self, index):
         '''
         ∂zᵢ/∂wᵢ = inputᵢ  -- the change in neuron output with respect to a weight
         '''
         return self.inputs[index]
 
-    def netWRTWeightVector (self):
+    def netWRTWeightVector(self):
         '''
         ∂zᵢ/∂wᵢ = inputᵢ  -- the change in neuron output with respect to a weight - this is a vector
         '''
@@ -67,118 +73,155 @@ class NeuralLayer:
 
     def psiWRTz(self, index):
         '''
-        ∂ψᵢ/∂zᵢ = ψᵢ*(1-ψᵢ) where ψ = 1 / (1 + e^(-z)) -- the partial change of ψ with respect to z - this is a scalar - must designate output index
+        ∂ψᵢ/∂zᵢ = ψᵢ*(1-ψᵢ) where ψ = 1 / (1 + e^(-z)) -- the partial change of ψ with respect to z - this
+        is a scalar - must designate output index
         '''
         return self.outputs[index]*(1 - self.outputs[index])
 
     def errorWRTPsi(self, targetArray, index):
         '''
-        ∂Eᵢ/∂ψᵢ =  -(targetOutput - ψᵢ)  # assuming that E is square of the error and ignoring the gain (2) - this is a scalar must designate output index
+        ∂Eᵢ/∂ψᵢ =  -(targetOutput - ψᵢ)  # assuming that E is square of the error and ignoring the gain (2) -
+        this is a scalar must designate output index
         '''
         if (self.backPropagatedErrorNotSet):
             targetOutput = targetArray[index]
-            self.error[index] = -(targetOutput - self.outputs[index])
-            #print("Measured Error: {}, index: {}, target output: {}, calculated output: {}".format(self.error[index], index, targetOutput, self.outputs[index]))
+            self.error[index] = - (self.learningFactor * (targetOutput - self.outputs[index])
+                                   * self.outputs[index] * (1.0 - self.outputs[index]))
         else:
             pass  # should have been set by a higher layer
         return self.error[index]
 
-    def updateWeights(self, target=None, deltas=None):
+    def updateWeights(self, top=False, target=None, deltas=None):
         '''
         Update the weights to minimize the loss - if in batch mode, the deltas have been accumulated by updateDeltas
         '''
-        #print("previous weights:\n {}".format(self.weights))
         if deltas is None:
-            for neuron in range(self.numberOfNeurons):
-                #print("Working on neuron index {}".format(neuron))
-                if neuron == 0:
-                    deltas = [self.errorWRTPsi(target, neuron) * self.psiWRTz(neuron) * self.netWRTWeightVector()]  # make a 1 by n vector
-                    #print("deltas: {}".format(deltas))
-                else:
-                    deltas = tf.concat([deltas, [self.errorWRTPsi(target, neuron) * self.psiWRTz(neuron) * self.netWRTWeightVector()]], 0)  # tack on a new row
-            self.propogateError()  # do this before updating weights and updateDeltas hasn't done this
-        #print("weights:\n{}".format(self.weights))
-        #print("type of deltas is: {}\n{}".format(type(deltas), deltas))
-        #print("transposed deltas:\n{}".format(tf.transpose(deltas)))
-        self.weights -= self.learningFactor * tf.transpose(deltas)
-        #print("final deltas:\n {}".format(tf.transpose(deltas)))
-        #print("new weights:\n {}".format(self.weights))
+            if top:
+                for neuron in range(self.numberOfNeurons):
+                    if neuron == 0:
+                        deltas = [self.errorWRTPsi(target, neuron)
+                                  * self.netWRTWeightVector()]  # make a 1 by n vector
+                    else:
+                        deltas = tf.concat([deltas, [self.errorWRTPsi(target, neuron)
+                                                     * self.netWRTWeightVector()]], 0)  # tack on a new row
+            else:
+                for neuron in range(self.numberOfNeurons):
+                    if neuron == 0:
+                        deltas = [self.errorWRTPsi(target, neuron)
+                                  * self.outputs[neuron]
+                                  * (1.0 - self.outputs[neuron])
+                                  * self.netWRTWeightVector()]  # make a 1 by n vector
+                    else:
+                        deltas = tf.concat([deltas, [self.errorWRTPsi(target, neuron)
+                                                     * self.outputs[neuron]
+                                                     * (1.0 - self.outputs[neuron])
+                                                     * self.netWRTWeightVector()]], 0)  # tack on a new row
+            self.propagateError()  # do this before updating weights and updateDeltas hasn't done this
+        self.weights -= tf.transpose(deltas)
 
-
-    def updateDeltas(self, target, deltas=None):
+    def updateDeltas(self, target, top=False, deltas=None):
         '''
         Update the deltas during batch processing
         '''
-        if deltas is None:
-            for neuron in range(self.numberOfNeurons):
-                #print("Working on neuron index {}".format(neuron))
-                if neuron == 0:
-                    deltas = tf.reshape(tf.convert_to_tensor(self.errorWRTPsi(target, neuron) * self.psiWRTz(neuron) * self.netWRTWeightVector()),[1, len(self.netWRTWeightVector())])  # make a 1 by n vector
-                    #print("deltas: {}".format(deltas))
-                else:
-                    deltas = tf.concat((deltas, [self.errorWRTPsi(target, neuron) * self.psiWRTz(neuron) * self.netWRTWeightVector()]), 0)  # tack on a new row
-                #print("neuron {}, type {}, deltas\n{}".format(neuron, type(deltas), deltas))
+        if top:
+            if deltas is None:
+                for neuron in range(self.numberOfNeurons):
+                    if neuron == 0:
+                        deltas = tf.reshape(tf.convert_to_tensor(self.errorWRTPsi(target, neuron)
+                                                                 * self.netWRTWeightVector()),
+                                            [1, len(self.netWRTWeightVector())])  # make a 1 by n vector
+                    else:
+                        deltas = tf.concat((deltas, [self.errorWRTPsi(target, neuron)
+                                                     * self.netWRTWeightVector()]), 0)  # tack on a new row
+                    if self.debug:
+                        print("updateDeltas - layer {}, neuron {}, weight deltas\n{}".
+                              format(self.id, neuron, deltas))
+            else:
+                for neuron in range(self.numberOfNeurons):
+                    if neuron == 0:
+                        deltaDeltas = tf.reshape(tf.convert_to_tensor(self.errorWRTPsi(target, neuron)
+                                                                      * self.netWRTWeightVector()),
+                                                 [1, len(self.netWRTWeightVector())])  # make a 1 by n vector
+                    else:
+                        deltaDeltas = tf.concat((deltaDeltas, [self.errorWRTPsi(target, neuron)
+                                                               * self.netWRTWeightVector()]), 0)  # tack on a new row
+                    if self.debug:
+                        print("updateDeltas - layer {}, neuron {}, weight deltaDeltas\n{}".
+                              format(self.id, neuron, deltaDeltas))
+                deltas += deltaDeltas
         else:
-            for neuron in range(self.numberOfNeurons):
-                #print("Working on neuron index {}".format(neuron))
-                if neuron == 0:
-                    deltaDeltas = tf.reshape(tf.convert_to_tensor(self.errorWRTPsi(target, neuron) * self.psiWRTz(neuron) * self.netWRTWeightVector()),[1, len(self.netWRTWeightVector())])  # make a 1 by n vector
-                    #print("deltas: {}".format(deltas))
-                else:
-                    deltaDeltas = tf.concat((deltaDeltas, [self.errorWRTPsi(target, neuron) * self.psiWRTz(neuron) * self.netWRTWeightVector()]), 0)  # tack on a new row
-                #print("neuron {}, type {}, deltaDeltas\n{}".format(neuron, type(deltaDeltas), deltaDeltas))
-            deltas += deltaDeltas
-        self.propogateError()  # do this before updating weights
-        #print("final deltas: {}".format(tf.transpose(deltas)))
-        #print("new weights: {}".format(self.weights))
+            if deltas is None:
+                for neuron in range(self.numberOfNeurons):
+                    if neuron == 0:
+                        deltas = tf.reshape(tf.convert_to_tensor(self.errorWRTPsi(target, neuron)
+                                                                 * self.outputs[neuron]
+                                                                 * (1.0 - self.outputs[neuron])
+                                                                 * self.netWRTWeightVector()),
+                                            [1, len(self.netWRTWeightVector())])  # make a 1 by n vector
+                    else:
+                        deltas = tf.concat((deltas, [self.errorWRTPsi(target, neuron)
+                                                     * self.outputs[neuron]
+                                                     * (1.0 - self.outputs[neuron])
+                                                     * self.netWRTWeightVector()]), 0)  # tack on a new row
+                    if self.debug:
+                        print("updateDeltas - layer {}, neuron {}, weight deltas\n{}".
+                              format(self.id, neuron, deltas))
+            else:
+                for neuron in range(self.numberOfNeurons):
+                    if neuron == 0:
+                        deltaDeltas = tf.reshape(tf.convert_to_tensor(self.errorWRTPsi(target, neuron)
+                                                                      * self.outputs[neuron]
+                                                                      * (1.0 - self.outputs[neuron])
+                                                                      * self.netWRTWeightVector()),
+                                                 [1, len(self.netWRTWeightVector())])  # make a 1 by n vector
+                    else:
+                        deltaDeltas = tf.concat((deltaDeltas, [self.errorWRTPsi(target, neuron)
+                                                               * self.outputs[neuron]
+                                                               * (1.0 - self.outputs[neuron])
+                                                               * self.netWRTWeightVector()]), 0)  # tack on a new row
+                    if self.debug:
+                        print("updateDeltas - layer {}, neuron {}, weight deltaDeltas\n{}".
+                              format(self.id, neuron, deltaDeltas))
+                deltas += deltaDeltas
+        self.propagateError()  # do this before updating weights
         return deltas
 
-    def propogateError(self):
+    def propagateError(self):
         '''
         Determine error to send to previous layers
         For each neuron, determine the amount of error at it's output that needs to be applied to the input
         which is the output of the previous level.  Those individual neuron amounts then need to be summed
-        across all neurons. 
+        across all neurons.
         '''
         previousLayerNeuronError = [0.0] * (self.numberOfInputs + 1)
         for thisLayerNeuron in range(self.numberOfNeurons):
             error = self.error[thisLayerNeuron]
             amountForEachPreviousLayerNeuron = error * self.weights[:, thisLayerNeuron]
+            if self.debug:
+                print("sum of weights for neurons at this layer: {}".
+                      format(tf.reduce_sum(self.weights[:, thisLayerNeuron])))
+                print("propagateError - in layer {}, neuron {}, contribution:{}".
+                      format(self.id, thisLayerNeuron, amountForEachPreviousLayerNeuron))
+                print("propagateError - Error {}, weights {}".format(error, self.weights[:, thisLayerNeuron]))
             previousLayerNeuronError += amountForEachPreviousLayerNeuron
-        #vectorError = tf.concat([self.error, [0.0]], 0)
-        #print("Error at this level:\n {}".format(vectorError))
-        #print("Weights being used for propogation:\n {}".format(self.weights))
-        #print("Propogated Error:\n {}".format(tf.tensordot(vectorError, self.weights, 1)))
-        #self.errorForNextLayer =  tf.tensordot(vectorError, self.weights, 1)
         self.errorForNextLayer = previousLayerNeuronError
-        #print("Propogated Error:\n {}".format(self.errorForNextLayer))
+        if self.debug:
+            print("propagateError - in layer {}, the next layer's error will be\n {}".
+                  format(self.id, previousLayerNeuronError))
 
-    def setPropogationError(self, error):
+    def setPropagationError(self, error):
         '''
         From a higher layer, set the error propogating back to this layer
         '''
         self.error = error
+        if self.debug:
+            print("setPropagationError - setting propagation error in layer {} to\n {}".
+                  format(self.id, self.error))
         self.backPropagatedErrorNotSet = False
-        
-def main():
-    '''
-    Tests for the class NeuralLayer
-    '''
-    twoByTwo = NeuralLayer(2, 2, 0.05)  #  make a layer with two neurons/inputs and two outputs
-    twoByTwo.calculateOutput([1.0, 1.0])
-    print("neuron outputs after this pass should be [0.5, 0.5] because e⁰ = 1", twoByTwo.outputs)
-    print("∂Eᵢ/∂w for neuron 0 {}".format(twoByTwo.errorWRTPsi([0.01, 0.99], 0)*twoByTwo.psiWRTz(0)* twoByTwo.netWRTWeightVector()))
-    print("∂Eᵢ/∂w for neuron 1 {}".format(twoByTwo.errorWRTPsi([0.01, 0.99], 1)*twoByTwo.psiWRTz(1)* twoByTwo.netWRTWeightVector()))
-    print("Initial weights:\n {}".format(twoByTwo.weights))
-    twoByTwo.updateWeights([0.01, 0.99])
-    # do another iteration
-    for count in range(10000):
-        twoByTwo.calculateOutput([1.0, 1.0])
-        twoByTwo.updateWeights([0.01, 0.99])
-    print
-    ("final weights:\n {}".format(twoByTwo.weights))
-    print("final outputs:\n {}".format(twoByTwo.outputs))
-    twoByTwo.propogateError()
-    print("Propogated Error:\n {}".format(twoByTwo.errorForNextLayer))
-if __name__ == '__main__':
-    main()
+
+    def setLearningFactor(self, factor):
+        '''
+        Setter for learning factor
+        '''
+        self.learningFactor = factor
+
